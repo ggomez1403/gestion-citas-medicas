aec97584ae6927d27285a188b347cb47f318e3bb
Geraldine Gómez <geralgomez1403@gmail.com>
2024-04-29 18:45:49 -0500

    Register - Login functionality and pages added

---
+++ src/app/core/models/User.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+  idNumber: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  role: string;
+}
---
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  constructor(
+    private readonly fb: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  public loginForm!: FormGroup;
+  public errorSession!: boolean;
+  private subscription!: Subscription;
+
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+    this.loginForm = this.initForm();
+
+    this.subscription = this.authService.errorLogin$.subscribe((error) => {
+      this.errorSession = error;
+    });
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    const { email, password } = this.loginForm.value;
+    this.authService.login(email, password);
+  }
+
+  get email(): AbstractControl | null {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl | null {
+    return this.loginForm.get('password');
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.authService.resetErrorSession();
+  }
+}
